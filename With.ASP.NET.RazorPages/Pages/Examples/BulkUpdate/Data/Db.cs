f3f39ef63a391cb18332aaea05ce4257b831656f
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2024-12-07 21:11:56 +0100

    BulkUpdate feature complete

---
+++ With.ASP.NET.RazorPages/Pages/Examples/BulkUpdate/Data/Db.cs
@@ -0,0 +1,32 @@
+﻿namespace With.ASP.NET.RazorPages.Pages.Examples.BulkUpdate.Data;
+
+public class Db
+{
+    public static List<Contact> Contacts =
+    [
+        new()
+        {
+            Name = "Joe Smith",
+            Email = "joe@smith.org",
+            Active = true
+        },
+        new()
+        {
+            Name = "Angie MacDowell",
+            Email = "angie@macdowell.org",
+            Active = true
+        },
+        new()
+        {
+            Name = "Fuqua Tarkenton",
+            Email = "fuqua@tarkenton.org",
+            Active = true
+        },
+        new()
+        {
+            Name = "Kim Yee",
+            Email = "kim@yee.org",
+            Active = false
+        }
+    ];
+}
