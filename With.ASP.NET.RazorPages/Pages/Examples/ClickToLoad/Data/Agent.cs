4805b50de05242fbc570e8fc644ce68e94aeb3f1
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2024-12-18 08:25:26 +0100

    ClickToLoad is feature complete

---
+++ With.ASP.NET.RazorPages/Pages/Examples/ClickToLoad/Data/Agent.cs
@@ -0,0 +1,20 @@
+namespace With.ASP.NET.RazorPages.Pages.Examples.ClickToLoad.Data;
+
+public class Agent
+{
+    public string Name { get; init; }
+    public string Email { get; init; }
+    public string ID { get; init; }
+
+    public static Agent[] CreateAgents(int page)
+    {
+        return Enumerable.Range(page * 10, 10)
+            .Select(x =>
+                new Agent
+                {
+                    Name = "Agent Smith",
+                    Email = $"void{x}@null.org",
+                    ID = Guid.NewGuid().ToString("N").ToUpperInvariant()
+                }).ToArray();
+    }
+}
---
+++ With.ASP.NET.RazorPages/Pages/Examples/ClickToLoad/Index.cshtml.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace With.ASP.NET.RazorPages.Pages.Examples.ClickToLoad;
+
+public class IndexModel : PageModel
+{
+    public void OnGet() { }
+}
