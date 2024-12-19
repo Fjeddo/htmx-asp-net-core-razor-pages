055d7e35ef49d766aebcdbd023d48f70985900bd
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2024-12-19 20:21:45 +0100

    Feature/bulk update (#1)
    Add bulk update example

---
+++ With.ASP.NET.RazorPages/Pages/Examples/BulkUpdate/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace With.ASP.NET.RazorPages.Pages.Examples.BulkUpdate
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
