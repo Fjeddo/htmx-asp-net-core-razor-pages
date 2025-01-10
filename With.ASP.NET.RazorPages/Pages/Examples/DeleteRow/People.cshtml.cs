80f0abdce53196d43824e138a3df5f59f80bbb11
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2025-01-10 12:33:52 +0100

    Delete row is feature complete

---
+++ With.ASP.NET.RazorPages/Pages/Examples/DeleteRow/People.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using With.ASP.NET.RazorPages.Pages.Examples.DeleteRow.Data;
+
+namespace With.ASP.NET.RazorPages.Pages.Examples.DeleteRow;
+
+public class PeopleModel : PageModel
+{
+    public List<Person> People { get; set; } = Db.People;
+
+    public void OnGet() { }
+
+    public IActionResult OnDelete(int id)
+    {
+        Db.People.RemoveAll(x => x.Id == id);
+        return Partial("_Empty");
+        ;        }
+}
