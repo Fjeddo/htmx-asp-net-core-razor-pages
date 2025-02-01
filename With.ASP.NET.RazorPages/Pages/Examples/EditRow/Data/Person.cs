3162a768c5dd325098bc27b6de6f4292719905a7
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2025-02-01 22:19:15 +0100

    Feature complete, working pretty good

---
+++ With.ASP.NET.RazorPages/Pages/Examples/EditRow/Data/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace With.ASP.NET.RazorPages.Pages.Examples.EditRow.Data;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Status { get; set; }
+}
