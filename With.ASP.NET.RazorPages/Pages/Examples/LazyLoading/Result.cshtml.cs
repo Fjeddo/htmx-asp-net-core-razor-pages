0a4a350adaca9766b54e0087d20e40463b76d5dc
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2025-02-02 20:10:12 +0100

    Feature/lazy loading (#8)
    * Feature complete lazy loading images from htmx.org
    * Some code cleanup missed earlier

---
+++ With.ASP.NET.RazorPages/Pages/Examples/LazyLoading/Result.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace With.ASP.NET.RazorPages.Pages.Examples.LazyLoading
+{
+    public class ResultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
