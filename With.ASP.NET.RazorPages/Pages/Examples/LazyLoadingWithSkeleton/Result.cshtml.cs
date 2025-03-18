97bcaec0c6e400a9ffc4faf5a0499eac4642a3d6
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2025-03-18 20:20:51 +0100

    Feature complete lazy loading with skeleton

---
+++ With.ASP.NET.RazorPages/Pages/Examples/LazyLoadingWithSkeleton/Result.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace With.ASP.NET.RazorPages.Pages.Examples.LazyLoadingWithSkeleton
+{
+    public class ResultModel : PageModel
+    {
+        public async Task OnGet()
+        {
+            await Task.Delay(3000);
+        }
+    }
+}
