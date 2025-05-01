e12477a1ba1d689cf54e8edfa2eacd54143a6ddf
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2025-05-01 16:46:50 +0200

    Make it possible to reset all in-mem data (#10)

---
+++ With.ASP.NET.RazorPages/Pages/Examples/DbBase.cs
@@ -0,0 +1,8 @@
+﻿namespace With.ASP.NET.RazorPages.Pages.Examples;
+
+public abstract class DbBase<T>
+{
+    protected static List<T>? TheList;
+
+    public static void Reset() => TheList = null;
+}
