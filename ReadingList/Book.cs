873b38b6c285941f64036a42887f3e3ddf813c14
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2025-05-13 22:38:29 +0200

    Reading list web app + pptx work

--- ReadingList/Book.cs
+++ ReadingList/Book.cs
@@ -4,28 +4,3 @@ public class Book
     public string title { get; set; }
     public string author { get; set; }
 }
-
-public class Db
-{
-    public static List<Book> Books { get; set; } =
-    [
-        new()
-        {
-            author = "Astrid L",
-            id = "C8B504C5-F1A0-43A6-B0C5-E01411F67655",
-            title = "Pippi Långstrump"
-        },
-        new()
-        {
-            author = "Selma L",
-            id = "E2F64680-AD0B-44C7-BFC1-2019BCF46FE9",
-            title = "Nils H"
-        },
-        new()
-        {
-            author = "Olov S",
-            id = "21EAB1EA-7439-4E58-9AE0-EDA2EC472255",
-            title = "Slottet brinner"
-        }
-    ];
-}
--- ReadingList/Pages/Index.cshtml.cs
+++ ReadingList/Pages/Index.cshtml.cs
@@ -1,54 +1,63 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ReadingList.Pages
-{
-    public class IndexModel : PageModel
-    {
-        private readonly ILogger<IndexModel> _logger;
+namespace ReadingList.Pages;
 
-        public IndexModel(ILogger<IndexModel> logger)
+public class IndexModel : PageModel
+{
+    public IActionResult OnGet(string id)
+    {
+        if (string.IsNullOrWhiteSpace(id))
         {
-            _logger = logger;
+            return Page();
         }
 
-        public IActionResult OnGet(string id)
+        if (id == "-1")
         {
-            if (string.IsNullOrWhiteSpace(id))
-            {
-                return Page();
-            }
-
             return Partial("_List", Db.Books.ToArray());
         }
 
-        public IActionResult OnPostSearch()
-        {
-            var search = Request.Form["search"];
-
-            return Partial("_List", Db.Books.Where(x => 
-                x.title.Contains(search, StringComparison.InvariantCultureIgnoreCase) ||
-                x.author.Contains(search, StringComparison.InvariantCultureIgnoreCase)).ToArray());
-        }
-        
-        public IActionResult OnPost()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IActionResult OnDelete(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IActionResult OnPut(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IActionResult OnGetEdit(string id)
-        {
-            throw new NotImplementedException();
-        }
+        return Partial("_Book", Db.Books.FirstOrDefault(x => x.id == id));
     }
+
+    public IActionResult OnPostSearch()
+    {
+        var search = Request.Form["search"];
+
+        return Partial("_List", Db.Books.Where(x => 
+            x.title.Contains(search, StringComparison.InvariantCultureIgnoreCase) ||
+            x.author.Contains(search, StringComparison.InvariantCultureIgnoreCase)).ToArray());
+    }
+        
+    public IActionResult OnPost()
+    {
+        var title = Request.Form["title"];
+        var author = Request.Form["author"];
+
+        var book = new Book {author = author, title = title, id = Guid.NewGuid().ToString()};
+        Db.Books.Add(book);
+
+        return RedirectToPage("Index", new {id = book.id});
+    }
+
+    public IActionResult OnDelete(string id)
+    {
+        Db.Books.RemoveAll(x => x.id == id);
+
+        return Partial("_Empty");
+    }
+
+    public IActionResult OnPut(string id)
+    {
+        var book = Db.Books.FirstOrDefault(x => x.id == id);
+        if (book != null)
+        {
+            book.author = Request.Form["author"];
+            book.title = Request.Form["title"];
+        }
+
+        return Partial("_Book", book);
+    }
+
+    public IActionResult OnGetEdit(string id) => Partial("_Edit", Db.Books.FirstOrDefault(x => x.id == id));
 }
