9101de3d92908d654fd31ed2c0f6ff2126dbd4c1
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2025-05-16 22:37:28 +0200

    Make more robust, not using action params etc.

---
+++ With.ASP.NET.RazorPages/Pages/Examples/ClickToEdit/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Htmx;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using With.ASP.NET.RazorPages.Pages.Examples.ClickToEdit.Data;
+
+namespace With.ASP.NET.RazorPages.Pages.Examples.ClickToEdit;
+
+public class IndexModel : PageModel
+{
+    public ContactInfo? ContactInfo { get; private set; }
+
+    public void OnGet(int id) => ContactInfo = Db.Contacts.FirstOrDefault(x => x.ID == id)!;
+
+    public IActionResult OnGetView(int id) => Partial("_View", Db.Contacts.FirstOrDefault(x => x.ID == id));
+
+    public IActionResult OnGetEdit(int id) => Partial("_Edit", Db.Contacts.FirstOrDefault(x => x.ID == id));
+
+    public IActionResult OnPut([FromForm] ContactInfo contactInfo)
+    {
+        Db.Upsert(contactInfo);
+        return Partial("_View", contactInfo);
+    }
+}
