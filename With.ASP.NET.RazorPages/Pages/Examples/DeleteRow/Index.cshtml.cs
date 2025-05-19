e307d1b5001245160b4e6b883d383b77b99ef4c1
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2025-05-19 21:21:20 +0200

    Refactor, use delete or outerHTML swapping (#16)

--- With.ASP.NET.RazorPages/Pages/Examples/DeleteRow/Index.cshtml.cs
+++ With.ASP.NET.RazorPages/Pages/Examples/DeleteRow/Index.cshtml.cs
@@ -4,13 +4,14 @@ using With.ASP.NET.RazorPages.Pages.Examples.DeleteRow.Data;
 
 namespace With.ASP.NET.RazorPages.Pages.Examples.DeleteRow;
 
-public class PeopleModel : PageModel
+public class IndexModel : PageModel
 {
     public List<Person> People { get; set; } = Db.People;
 
     public IActionResult OnDelete(int id)
     {
         Db.People.RemoveAll(x => x.Id == id);
-        return Partial("_Empty");
+        //return Partial("_Empty"); // Used with hx-swap=outerHTML
+        return new OkResult(); // Used with hx-swap=delete
     }
 }
