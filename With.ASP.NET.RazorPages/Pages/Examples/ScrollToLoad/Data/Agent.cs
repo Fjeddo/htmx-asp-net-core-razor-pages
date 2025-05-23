c58b753c8114279e45ff53d02440f4a31ff290f8
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2025-05-23 22:15:14 +0200

    Scroll to load is feature complete (#17)

---
+++ With.ASP.NET.RazorPages/Pages/Examples/ScrollToLoad/Data/Agent.cs
@@ -0,0 +1,20 @@
+namespace With.ASP.NET.RazorPages.Pages.Examples.ScrollToLoad.Data;
+
+public class Agent
+{
+    public string Name { get; init; }
+    public string Email { get; init; }
+    public string ID { get; init; }
+
+    public static Agent[] GetAgents(int page)
+    {
+        return Enumerable.Range((page + 1) * 10, 10)
+            .Select(x =>
+                new Agent
+                {
+                    Name = "Agent Smith",
+                    Email = $"void{x}@null.org",
+                    ID = Guid.NewGuid().ToString("N").ToUpperInvariant()
+                }).ToArray();
+    }
+}
